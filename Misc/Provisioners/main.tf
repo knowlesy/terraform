92dbead69fc82886fe6495f983bbb9c01d21f2ae
PK <pk@noemailaddress.com>
2024-04-03 22:13:42 +0100

    update

--- Misc/Provisioners/main.tf
+++ Misc/Provisioners/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "us-east-1"
+  region                      = "us-east-1"
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
@@ -9,20 +9,36 @@ provider "aws" {
 }
 
 resource "aws_instance" "myec2" {
-    ami = "ami-00c39f71452c08778"
-    instance_type = "t2.micro"
-    key_name   = "deployer-key"
-    connection {
-    type     = "ssh"
-    user     = "root"
+  ami           = "ami-00c39f71452c08778"
+  instance_type = "t2.micro"
+  key_name      = "deployer-key"
+  connection {
+    type        = "ssh"
+    user        = "root"
     private_key = file("./deployer-key.pem")
-    host     = self.public_ip
+    host        = self.public_ip
   }
 
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update && sudo apt-get upgrade -y"
     ]
+  }
+}
+
+resource "aws_instance" "myec2-2" {
+  ami           = "ami-00c39f71452c08778"
+  instance_type = "t2.micro"
+  key_name      = "deployer-key"
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = file("./deployer-key.pem")
+    host        = self.public_ip
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${self.private_ip} >> private_ips.txt"
   }
 }
 
