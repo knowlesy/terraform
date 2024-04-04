9dc99422db1f11150e54499c69609e438b2c9035
PK <pk@noemailaddress.com>
2024-04-04 21:41:02 +0100

    locals in module

---
+++ Misc/mod_demo_3/modules/securitygroup/sg.tf
@@ -0,0 +1,28 @@
+locals {
+  TLS = "443"
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = local.TLS
+  ip_protocol       = "tcp"
+  to_port           = local.TLS
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = local.TLS
+  ip_protocol       = "tcp"
+  to_port           = local.TLS
+}
