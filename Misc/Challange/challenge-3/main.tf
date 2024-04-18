c3c8abe9a404287710edfaa920be5aff34d4be3e
“Pete <20459678+knowlesy@users.noreply.github.com>
2024-04-18 22:28:00 +0100

    fix

--- Misc/Challange/challenge-3/main.tf
+++ Misc/Challange/challenge-3/main.tf
@@ -3,7 +3,13 @@ variable "instance_config" {
   type = map
   default = {
     instance1 = { instance_type = "t2.micro", ami = "ami-03a6eaae9938c858c" }
-    instance2 = { instance_type = "t2.small", ami = "ami-053b0d53c279acc90" }
+    #instance2 = { instance_type = "t2.small", ami = "ami-053b0d53c279acc90" }
   }
 }
 
+resource "aws_instance" "web" {
+  for_each = var.instance_config
+  ami = each.value.ami
+  instance_type = each.value.instance_type
+  
+}
