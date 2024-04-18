eb14ce6eb09c5b9f9ee9c3edf55f5622417623a4
“Pete <20459678+knowlesy@users.noreply.github.com>
2024-04-18 22:49:56 +0100

    fix

---
+++ Misc/Challange/challenge-4/main.tf
@@ -0,0 +1,27 @@
+
+provider "aws" {
+  region = "us-east-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+data "aws_iam_users" "users" {}
+
+data "aws_called_identity" "current" {}
+
+resource "aws_iam_user" "lb" {
+  name = "admin-user-${data.aws_called_identity.id}"
+  path = "/system/"
+}
+
+output "user_names" {
+  value = data.aws_iam_users.users
+}
+
+output "total_users" {
+  value = length(data.aws_iam_users.users)
+}
+
