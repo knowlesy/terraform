31f7155482e7888eb57c7c97f6be0265c1311ab1
Pete K <20459678+knowlesy@users.noreply.github.com>
2024-11-26 16:41:12 +0000

    Add files via upload

--- Misc/mod-provider/main.tf
+++ Misc/mod-provider/main.tf
@@ -1,1 +1,21 @@
+resource "random_pet" "server" {
 
+}
+
+
+
+module "subnet_addrs" {
+  source = "hashicorp/subnets/cidr"
+
+  base_cidr_block = var.test
+  networks = [
+    {
+      name     = random_pet.server.id
+      new_bits = 8
+    },
+  ]
+}
+
+output "TestNetworkName" {
+  value = module.subnet_addrs.networks
+}
---
+++ Misc/mod-provider/variables.tf
@@ -0,0 +1,4 @@
+variable "test" {
+type = string
+default = "10.0.0.0/8"
+}
